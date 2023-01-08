86f02b3a0ee861ef928287c4a76a4c85000c3806
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-01-08 11:02:37 -0500

    commit tree ee23aca58be734ba47de741701c142e6ec6acfec

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main").remove();
+
+const newHeader = document.createElement('h1');
+// h1.id = 'victory';
+newHeader.id = 'victory';
+
+newHeader.textContent = "Jacki is the champion"
